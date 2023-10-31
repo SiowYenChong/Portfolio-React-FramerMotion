2c12a03b4d15c4dcba49cbef5eb0fa013919ced8
SiowYenC <Clairechong998@gmail.com>
2023-10-31 16:31:24 +0800

    completed blogpage with no animation

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -14,7 +14,7 @@ export const Blogs = [
         name:"How to create React Hamburger Menu using Styled-Components",
         tags:["react","styled-components"],
         date:"5 July, 2021",
-        imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625479747640%2F7KzwP9nmj.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+        imgSrc:"https://cdn.hashnode.com/res/hashnode/image/upload/v1625479747640/7KzwP9nmj.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
         link:"https://codebucks.hashnode.dev/react-hamburger-menu"
     },
     {
@@ -22,7 +22,7 @@ export const Blogs = [
         name:"React Loading Screen: Try these 3 cool loading screens for your app",
         tags:["react","react-lottie","styled-components"],
         date:"6 July, 2021",
-        imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625552344293%2Fegwis0UIX.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
         link:"https://codebucks.hashnode.dev/react-loading-screens"
     },
   
