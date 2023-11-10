eeecfe01bb227854e112c8ca988eba16a32d7950
SiowYenC <Clairechong998@gmail.com>
2023-11-11 02:23:05 +0800

    Adjusted content - blog

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -3,27 +3,36 @@
 export const Blogs = [
     {
         id:1,
-        name:"Build Website with ReactJS, Styled-components and GSAP for Scrolling Animations",
-        tags:["react","gsap","styled-components"],
-        date:"13 May, 2021",
-        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
-        link:"https://dev.to/codebucks/build-website-with-reactjs-styled-components-and-gsap-for-scrolling-animations-2f10"
+        name:"My Real-time LinkedIn feed",
+        tags:["LinkedIn","Connections","Feed"],
+        date:"11 Nov, 2023",
+        imgSrc:"https://media.licdn.com/dms/image/C5622AQG1lZJFFYCMRg/feedshare-shrink_800/0/1670138096201?e=1702512000&v=beta&t=wYL3WesKwbVMAUkLcL9mEJIJGuMGfOYDGu8CXi_9pks",
+        link:"https://kz.linkedin.com/posts/chong-siow-yen_iwd2023-daretobe-womenintech-activity-7043198909610946561-xmn1"
     },
     {
         id:2,
-        name:"How to create React Hamburger Menu using Styled-Components",
-        tags:["react","styled-components"],
-        date:"5 July, 2021",
-        imgSrc:"https://cdn.hashnode.com/res/hashnode/image/upload/v1625479747640/7KzwP9nmj.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
-        link:"https://codebucks.hashnode.dev/react-hamburger-menu"
+        name:"FYP Poster Contest - Champion",
+        tags:["Sign Recognition","Tensorflow","OpenCV","Python"],
+        date:"19 Sept, 2023",
+        imgSrc:"https://news.utar.edu.my/news/2023/Oct/06/02/16B.jpg",
+        link:"https://news.utar.edu.my/news/2023/Oct/06/02/02.html"
     },
     {
         id:3,
-        name:"React Loading Screen: Try these 3 cool loading screens for your app",
-        tags:["react","react-lottie","styled-components"],
-        date:"6 July, 2021",
-        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
-        link:"https://codebucks.hashnode.dev/react-loading-screens"
+        name:"Hackathon winning featured in Samsung Newsroom",
+        tags:["UI/UX","GalaxyZFold", "GalaxyZFlip","Samsung"],
+        date:"15 June, 2021",
+        imgSrc:"https://news.utar.edu.my/awards/2023/June/21/01/04.jpg",
+        link:"https://news.samsung.com/my/samsung-awards-young-talent-in-developer-day-hackathon-to-reimagine-foldable-experiences-for-malaysians"
     },
+    {
+        id:4,
+        name:"My last serving/AGM at UTAR IT Society Chairperson",
+        tags:["Leadership","UTAR", "IT Society"],
+        date:"9 July, 2022",
+        imgSrc:"https://scontent.fkul1-1.fna.fbcdn.net/v/t39.30808-6/292201727_2153939041447324_8579913064182737453_n.jpg?stp=dst-jpg_p960x960&_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=tlEfP0rGOvIAX-k4HSl&_nc_ht=scontent.fkul1-1.fna&oh=00_AfDAM5iuqrLawHS3dJBcXKyJ-OgxBTXWZChEoCfNEK3b1w&oe=65541579",
+        link:"https://www.facebook.com/146676765506905/posts/pfbid035DSjHJvQj4iXeahFBYH8VYUmnUmypsT6JmYPRUteVT2h2JLfT2XmvSgju66aWCDJl/?app=fbl"
+    },
+    
   
 ] 
